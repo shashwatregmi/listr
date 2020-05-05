c81e8731958f57332c624c20e9556aa45e66cddf
shashwatregmi <shashwat.regmi@icloud.com>
2020-05-04 23:22:28 -0700

    Basic UI

---
+++ listr/listr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listr
+//
+//  Created by Regmi on 2020-05-04.
+//  Copyright © 2020 Regmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
