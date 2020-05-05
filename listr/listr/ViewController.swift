8a822d6a023839771544d1cbf13f05183c8ded99
shashwatregmi <shashwat.regmi@icloud.com>
2020-05-04 23:33:48 -0700

    Action Controls

--- listr/listr/ViewController.swift
+++ listr/listr/ViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameTaskField: UITextField!
+    
+    @IBOutlet weak var taskNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTaskField.delegate = self
+    }
+    // UI Text Field Delegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        taskNameLabel.text = textField.text
     }
 
-
+    // Event here:
+    @IBAction func setDefaultLabel(_ sender: Any) {
+        taskNameLabel.text = "Task Name"
+    }
 }
