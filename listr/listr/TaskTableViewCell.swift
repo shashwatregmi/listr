3738280d195f402ab658a8cbee0f409765e33f85
shashwatregmi <shashwat.regmi@icloud.com>
2020-05-06 23:39:00 -0700

    Created Home Screen
    Created table screen to hold all of the tasks.

---
+++ listr/listr/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  listr
+//
+//  Created by Regmi on 2020-05-06.
+//  Copyright © 2020 Regmi. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
